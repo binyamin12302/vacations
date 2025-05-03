53b8e29cdd7e1d4758821f01a79a835b587bf6a9
binyamin12302 <binyamin12302@gmail.com>
2025-05-03 10:54:26 +0300

    fix build

--- Backend/build/2-utils/dal.js
+++ Backend/build/2-utils/dal.js
@@ -3,10 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const mysql_1 = __importDefault(require("mysql"));
+const mysql2_1 = __importDefault(require("mysql2"));
 const config_1 = __importDefault(require("./config"));
 // Create pool of connection and supply one when needed: 
-const connection = mysql_1.default.createPool({
+const connection = mysql2_1.default.createPool({
     host: config_1.default.sqlHost, // computer name
     user: config_1.default.sqlUser, // database username
     password: config_1.default.sqlPassword, // database password
