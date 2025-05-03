c7c8ac1bdafc5ace51287d2c76aee367d7a08491
binyamin12302 <binyamin12302@gmail.com>
2025-05-03 10:06:54 +0300

    fix: update CORS handling for production

--- Backend/build/app.js
+++ Backend/build/app.js
@@ -12,8 +12,15 @@ const errors_model_1 = require("./4-models/errors-model");
 const socket_logic_1 = __importDefault(require("./5-logic/socket-logic"));
 const auth_controller_1 = __importDefault(require("./6-controllers/auth-controller"));
 const vacations_controller_1 = __importDefault(require("./6-controllers/vacations-controller"));
+console.log("CORS enabled for: " + process.env.NODE_ENV);
 const expressServer = (0, express_1.default)();
-expressServer.use((0, cors_1.default)());
+const clientUrl = process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://vacations-three.vercel.app";
+expressServer.use((0, cors_1.default)({
+    origin: clientUrl,
+    credentials: true
+}));
 expressServer.use(express_1.default.json());
 // expressServer.use(expressFileUpload());
 expressServer.use((0, express_fileupload_1.default)({ useTempFiles: true }));
@@ -25,5 +32,5 @@ expressServer.use("*", (request, response, next) => {
     next(err);
 });
 expressServer.use(catch_all_1.default);
-const httpServer = expressServer.listen(config_1.default.port, () => console.log(`Listening....`));
+const httpServer = expressServer.listen(config_1.default.port, () => console.log("Server running on port", config_1.default.port));
 socket_logic_1.default.init(httpServer);
