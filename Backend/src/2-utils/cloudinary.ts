7944152cf03e027e9c6a5ff44aa7effdcd0134e2
binyamin12302 <binyamin12302@gmail.com>
2025-05-03 22:43:38 +0300

    Various updates and improvements

--- Backend/src/2-utils/cloudinary.ts
+++ Backend/src/2-utils/cloudinary.ts
@@ -6,4 +6,8 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+console.log("Cloud name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+console.log("API Secret:", process.env.CLOUDINARY_API_SECRET);
+
 export default cloudinary;
